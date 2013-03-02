e29ec811ee11c49e1f52dbd5058b3ec44f6b0c91
Doug Tykocki <tykockda@gmail.com>
2013-03-02 11:29:34 -0500

    Fix spacing

--- app/view_controllers/master_view_controller.rb
+++ app/view_controllers/master_view_controller.rb
@@ -1,45 +1,45 @@
 class MasterViewController < UITableViewController
-	CELLID = "CustomCell"
+  CELLID = "CustomCell"
 
-	def dateFormatter
-		@dateFormatter ||= NSDateFormatter.alloc.init
-		@dateFormatter.dateFormat = "dd MMM YYYY"
-		@dateFormatter
-	end
+  def dateFormatter
+    @dateFormatter ||= NSDateFormatter.alloc.init
+    @dateFormatter.dateFormat = "dd MMM YYYY"
+    @dateFormatter
+  end
 
-	def viewDidLoad
-		color = UIColor.colorWithRed(0.650, green:0.222, blue:0.750, alpha:1.000)
-		background_color = UIColor.colorWithWhite(0.25, alpha:1.0)
+  def viewDidLoad
+    color = UIColor.colorWithRed(0.650, green:0.222, blue:0.750, alpha:1.000)
+    background_color = UIColor.colorWithWhite(0.25, alpha:1.0)
 
-		self.title = "Custom Cells"
-		self.navigationController.navigationBar.tintColor = color
+    self.title = "Custom Cells"
+    self.navigationController.navigationBar.tintColor = color
 
-		self.tableView.backgroundColor = background_color
-		self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone
-		self.tableView.registerClass(CustomCell, forCellReuseIdentifier:CELLID)
-	end
+    self.tableView.backgroundColor = background_color
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone
+    self.tableView.registerClass(CustomCell, forCellReuseIdentifier:CELLID)
+  end
 
-	def tableView(tableView, numberOfRowsInSection:secton)
-		100
-	end
+  def tableView(tableView, numberOfRowsInSection:secton)
+    100
+  end
 
-	def tableView(tableView, heightForRowAtIndexPath:indexPath)
-		44
-	end
+  def tableView(tableView, heightForRowAtIndexPath:indexPath)
+    44
+  end
 
-	def tableView(tableView, cellForRowAtIndexPath:indexPath)
-		interval = (60 * 60 * 24) * indexPath.row
-		date = NSDate.date.dateByAddingTimeInterval(interval)
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    interval = (60 * 60 * 24) * indexPath.row
+    date = NSDate.date.dateByAddingTimeInterval(interval)
 
-		cell = tableView.dequeueReusableCellWithIdentifier(CELLID) 
-		cell.mainTextLabel.text = self.dateFormatter.stringFromDate(date)
-		cell.subTextLabel.text = "#{rand(999).to_s} items"
+    cell = tableView.dequeueReusableCellWithIdentifier(CELLID) 
+    cell.mainTextLabel.text = self.dateFormatter.stringFromDate(date)
+    cell.subTextLabel.text = "#{rand(999).to_s} items"
 
-		cell
-	end
+    cell
+  end
 
-	def tableView(tableView, didSelectRowAtIndexPath:indexPath)
-		tableView.deselectRowAtIndexPath(indexPath, animated:true)		
-	end
+  def tableView(tableView, didSelectRowAtIndexPath:indexPath)
+    tableView.deselectRowAtIndexPath(indexPath, animated:true)    
+  end
 
 end
