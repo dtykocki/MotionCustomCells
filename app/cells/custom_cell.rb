401f46da1282047ef484fc53baaf5d9426e20c52
Doug Tykocki <tykockda@gmail.com>
2013-03-02 11:29:03 -0500

    fix spacing

--- app/cells/custom_cell.rb
+++ app/cells/custom_cell.rb
@@ -1,81 +1,81 @@
 class CustomCell < UITableViewCell
-	HORIZONTAL_MARGIN = 10
-	VERTICAL_MARGIN = 4
+  HORIZONTAL_MARGIN = 10
+  VERTICAL_MARGIN = 4
 
-	attr_accessor :mainTextLabel, :subTextLabel
+  attr_accessor :mainTextLabel, :subTextLabel
 
-	def initWithStyle(style, reuseIdentifier:reuseIdentifier)
-		if super	
-			addMainLabel
-			addSubLabel
-			setupBackground
-		end
+  def initWithStyle(style, reuseIdentifier:reuseIdentifier)
+    if super  
+      addMainLabel
+      addSubLabel
+      setupBackground
+    end
 
-		self
-	end
+    self
+  end
 
-	def layoutSubviews
-		super	
+  def layoutSubviews
+    super 
 
-		text_size = @mainTextLabel.text.sizeWithFont(@mainTextLabel.font)
-		x, y			= HORIZONTAL_MARGIN, VERTICAL_MARGIN
-		width			= text_size.width
-		height		= self.contentView.frame.size.height - (2 * VERTICAL_MARGIN)
-		@mainTextLabel.frame = [[x,y],[width,height]]
+    text_size = @mainTextLabel.text.sizeWithFont(@mainTextLabel.font)
+    x, y      = HORIZONTAL_MARGIN, VERTICAL_MARGIN
+    width     = text_size.width
+    height    = self.contentView.frame.size.height - (2 * VERTICAL_MARGIN)
+    @mainTextLabel.frame = [[x,y],[width,height]]
 
-		text_size = @subTextLabel.text.sizeWithFont(@subTextLabel.font)
-		x					+= width + HORIZONTAL_MARGIN
-		width			= text_size.width
-		
-		@subTextLabel.frame = [[x,y],[width,height]]
-	end
+    text_size = @subTextLabel.text.sizeWithFont(@subTextLabel.font)
+    x         += width + HORIZONTAL_MARGIN
+    width     = text_size.width
+    
+    @subTextLabel.frame = [[x,y],[width,height]]
+  end
 
-	def setSelected(selected, animated:animated)
-		super	
-	end
+  def setSelected(selected, animated:animated)
+    super 
+  end
 
-	def setHighlighted(highlighted, animated:animated)
-		super	
-		shadow_offset = highlighted ? [0,0] : [0,1]
+  def setHighlighted(highlighted, animated:animated)
+    super 
+    shadow_offset = highlighted ? [0,0] : [0,1]
 
-		@mainTextLabel.shadowOffset = shadow_offset
-		@subTextLabel.shadowOffset = shadow_offset
-	end
+    @mainTextLabel.shadowOffset = shadow_offset
+    @subTextLabel.shadowOffset = shadow_offset
+  end
 
-	private
+  private
 
-	def addMainLabel
-		@mainTextLabel = UILabel.alloc.init
-		@mainTextLabel.font = UIFont.boldSystemFontOfSize(16)
-		@mainTextLabel.textColor = UIColor.colorWithWhite(0.45, alpha:1.0)
-		@mainTextLabel.shadowColor = UIColor.whiteColor
-		@mainTextLabel.shadowOffset = [0, 1]
-		@mainTextLabel.backgroundColor = UIColor.clearColor
-		@mainTextLabel.highlightedTextColor = UIColor.whiteColor
+  def addMainLabel
+    @mainTextLabel = UILabel.alloc.init
+    @mainTextLabel.font = UIFont.boldSystemFontOfSize(16)
+    @mainTextLabel.textColor = UIColor.colorWithWhite(0.45, alpha:1.0)
+    @mainTextLabel.shadowColor = UIColor.whiteColor
+    @mainTextLabel.shadowOffset = [0, 1]
+    @mainTextLabel.backgroundColor = UIColor.clearColor
+    @mainTextLabel.highlightedTextColor = UIColor.whiteColor
 
-		self.contentView.addSubview(@mainTextLabel)
-	end
+    self.contentView.addSubview(@mainTextLabel)
+  end
 
-	def addSubLabel
-		@subTextLabel = UILabel.alloc.init
-		@subTextLabel.font = UIFont.systemFontOfSize(14)
-		@subTextLabel.textColor = UIColor.colorWithWhite(0.55, alpha:1.0)
-		@subTextLabel.shadowColor = UIColor.whiteColor
-		@subTextLabel.shadowOffset = [0, 1]
-		@subTextLabel.backgroundColor = UIColor.clearColor
-		@subTextLabel.highlightedTextColor = UIColor.whiteColor
+  def addSubLabel
+    @subTextLabel = UILabel.alloc.init
+    @subTextLabel.font = UIFont.systemFontOfSize(14)
+    @subTextLabel.textColor = UIColor.colorWithWhite(0.55, alpha:1.0)
+    @subTextLabel.shadowColor = UIColor.whiteColor
+    @subTextLabel.shadowOffset = [0, 1]
+    @subTextLabel.backgroundColor = UIColor.clearColor
+    @subTextLabel.highlightedTextColor = UIColor.whiteColor
 
-		self.contentView.addSubview(@subTextLabel)
-	end
+    self.contentView.addSubview(@subTextLabel)
+  end
 
-	def setupBackground
-		background = UIImage.imageNamed("cell-bg.png")
-		background_view = UIImageView.alloc.initWithImage(background)
-		self.backgroundView = background_view
+  def setupBackground
+    background = UIImage.imageNamed("cell-bg.png")
+    background_view = UIImageView.alloc.initWithImage(background)
+    self.backgroundView = background_view
 
-		highlight = UIImage.imageNamed("cell-bg-highlighted")
-		highlight_view = UIImageView.alloc.initWithImage(highlight)
-		self.selectedBackgroundView = highlight_view
-	end
+    highlight = UIImage.imageNamed("cell-bg-highlighted")
+    highlight_view = UIImageView.alloc.initWithImage(highlight)
+    self.selectedBackgroundView = highlight_view
+  end
 
 end
