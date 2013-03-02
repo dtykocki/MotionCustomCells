585b1847c628dbdfb0f96d91f4e8b8f9dc1c1e42
Doug Tykocki <tykockda@gmail.com>
2013-03-02 11:27:09 -0500

    Fix spacing

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,11 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-		masterViewController = MasterViewController.alloc.init
+    masterViewController = MasterViewController.alloc.init
 
-		@window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-		@window.rootViewController = UINavigationController.alloc.initWithRootViewController(masterViewController)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(masterViewController)
     @window.rootViewController.wantsFullScreenLayout = true
-		@window.makeKeyAndVisible
+    @window.makeKeyAndVisible
     true
   end
 end
