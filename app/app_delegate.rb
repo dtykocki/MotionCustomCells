01b8d952e28d6a11c79adb0a4e21e5c25f888932
Doug Tykocki <tykockda@gmail.com>
2013-03-02 10:49:23 -0500

    initial commit

---
+++ app/app_delegate.rb
@@ -0,0 +1,11 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+		masterViewController = MasterViewController.alloc.init
+
+		@window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+		@window.rootViewController = UINavigationController.alloc.initWithRootViewController(masterViewController)
+    @window.rootViewController.wantsFullScreenLayout = true
+		@window.makeKeyAndVisible
+    true
+  end
+end
